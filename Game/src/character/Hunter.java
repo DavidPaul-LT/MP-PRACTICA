e604d7c884b63979fd75b4e572b62172141c809b
iireeneeps012 <89156022+iireeneeps012@users.noreply.github.com>
2023-04-25 23:20:05 +0200

    Delete Game directory

--- Game/src/character/Hunter.java
+++
@@ -1,7 +0,0 @@
-package character;
-
-public class Hunter extends Character{
-    private final int MIN_WILL = 0;
-    private final int MAX_WILL = 3;
-    private int willPower;
-}
--- Game/src/minion/DemonFactory.java
+++
@@ -1,8 +0,0 @@
-package minion;
-
-public class DemonFactory extends MinionFactory{
-    @Override
-    public Minion create() {
-        return super.create();
-    }
-}
