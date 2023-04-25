4b2223b9ba37bf8b68648c6239edb529e004d094
Cristian <cav.contact.pr@gmail.com>
2023-04-25 23:57:32 +0200

    Branch integrity test

--- Game/src/screen/CambatHistoryScreen.java
+++ Game/src/screen/CambatHistoryScreen.java
@@ -1,9 +1,20 @@
 package screen;
 
+import interfaces.Screen;
 import user.Client;
 
-public class CambatHistoryScreen {
+public class CambatHistoryScreen implements Screen {
     public CambatHistoryScreen(Client user) {
         //TODO CombatHistoryScreen
+    }
+
+    @Override
+    public void getOption() {
+
+    }
+
+    @Override
+    public void loadOptions() {
+
     }
 }
