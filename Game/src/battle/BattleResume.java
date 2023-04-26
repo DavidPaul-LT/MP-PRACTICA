82f2471911470ef6c67f81c6d24362f8cb0fbc84
Cristian <cav.contact.pr@gmail.com>
2023-04-26 15:44:29 +0200

    Firsr commit of BattleResume related classes

--- Game/src/battle/BattleResume.java
+++ Game/src/battle/BattleResume.java
@@ -1,7 +1,45 @@
 package battle;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class BattleResume implements Serializable {
-    public void updateBattleResume(){}
+    private final String challenging;
+    private final String challenged;
+    private final Date combatDate;
+    private final String winner;
+    private final int minionsNotDefeated;
+    private final int goldWon;
+    public BattleResume(String challenging, String challenged, Date combatDate, String winner,int minionsNotDefeated,int goldWon){
+        this.challenging = challenging;
+        this.challenged = challenged;
+        this.combatDate = combatDate;
+        this.winner = winner;
+        this.minionsNotDefeated = minionsNotDefeated;
+        this.goldWon = goldWon;
+    }
+
+    public String getChallenging() {
+        return challenging;
+    }
+
+    public String getChallenged() {
+        return challenged;
+    }
+
+    public Date getCombatDate() {
+        return combatDate;
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public int getMinionsNotDefeated() {
+        return minionsNotDefeated;
+    }
+
+    public int getGoldWon() {
+        return goldWon;
+    }
 }
